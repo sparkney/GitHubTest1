6c7d0a30d2ea4cb43862d6595959267fc9b774af
orjan <orjan@sparkney.se>
2018-10-02 14:32:14 +0200

    Another commit

--- src/githubtest1/GitHubTest1.java
+++ src/githubtest1/GitHubTest1.java
@@ -15,7 +15,7 @@ public class GitHubTest1{
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub, locally!");
     }
     
 }
