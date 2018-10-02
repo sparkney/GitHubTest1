e91cd4da496bb79b879e0bfff56f4fb08c5dba80
sparkney <43208520+sparkney@users.noreply.github.com>
2018-10-02 14:29:59 +0200

    Update GitHubTest1.java

--- src/githubtest1/GitHubTest1.java
+++ src/githubtest1/GitHubTest1.java
@@ -16,6 +16,7 @@ public class GitHubTest1{
      */
     public static void main(String[] args){
         System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub remotly");
     }
     
 }
